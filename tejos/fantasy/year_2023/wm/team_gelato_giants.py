9455d98e3a7c8d4c29e57f6ef0345f9276acc3ce
Juki <jude@fauve.io>
2023-07-12 13:44:37 +1200

    Update team_gelato_giants.py

--- tejos/fantasy/year_2023/wm/team_gelato_giants.py
+++ tejos/fantasy/year_2023/wm/team_gelato_giants.py
@@ -39,19 +39,19 @@ def team_gelato_giants(mens_singles, womens_singles):
 
 # womens_singles_round_5:START
 def womens_singles_round_5(womens_singles):
-    TEAM.draw(womens_singles).match('5.1').winner(women).in_sets()  # (  1) Iga Swiatek  OR  ( WC) Svitolina
-    TEAM.draw(womens_singles).match('5.2').winner(women).in_sets()  # (  4) Jessica Pegula  OR  (   ) Marketa Vondrousova
-    TEAM.draw(womens_singles).match('5.3').winner(women).in_sets()  # (  6) Ons Jabeur  OR  (  3) Elena Rybakina
-    TEAM.draw(womens_singles).match('5.4').winner(women).in_sets()  # ( 25) Madison Keys  OR  (  2) Aryna Sabalenka
+    TEAM.draw(womens_singles).match('5.1').winner(women.Svitolina).in_sets(3)  # (  1) Iga Swiatek  OR  ( WC) Svitolina
+    TEAM.draw(womens_singles).match('5.2').winner(women.Vondrousova).in_sets(3)  # (  4) Jessica Pegula  OR  (   ) Marketa Vondrousova
+    TEAM.draw(womens_singles).match('5.3').winner(women.Jabeur).in_sets(3)  # (  6) Ons Jabeur  OR  (  3) Elena Rybakina
+    TEAM.draw(womens_singles).match('5.4').winner(women.Sabalenka).in_sets(3)  # ( 25) Madison Keys  OR  (  2) Aryna Sabalenka
 # womens_singles_round_5:END
 
 
 # mens_singles_round_5:START
 def mens_singles_round_5(mens_singles):
-    TEAM.draw(mens_singles).match('5.1').winner(men).in_sets()  # (  1) Carlos Alcaraz  OR  (  6) Holger Rune
-    TEAM.draw(mens_singles).match('5.2').winner(men).in_sets()  # (  3) Daniil Medvedev  OR  (   ) Christopher Eubanks
-    TEAM.draw(mens_singles).match('5.3').winner(men).in_sets()  # (  8) Jannik Sinner  OR  (   ) Roman Safiullin
-    TEAM.draw(mens_singles).match('5.4').winner(men).in_sets()  # (  7) Andrey Rublev  OR  (  2) Novak Djokovic
+    TEAM.draw(mens_singles).match('5.1').winner(men.Alcaraz).in_sets(4)  # (  1) Carlos Alcaraz  OR  (  6) Holger Rune
+    TEAM.draw(mens_singles).match('5.2').winner(men.Eubanks).in_sets(5)  # (  3) Daniil Medvedev  OR  (   ) Christopher Eubanks
+    TEAM.draw(mens_singles).match('5.3').winner(men.Sinner).in_sets(4)  # (  8) Jannik Sinner  OR  (   ) Roman Safiullin
+    TEAM.draw(mens_singles).match('5.4').winner(men.Rublev).in_sets(4)  # (  7) Andrey Rublev  OR  (  2) Novak Djokovic
 # mens_singles_round_5:END
 
 
